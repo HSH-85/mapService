16f300496b1da75c2629818bdf708a75b06e4f98
HSH-85 <gjtjdgns@gmail.com>
2025-02-17 14:24:01 +0900

    전체완성

--- src/main/java/com/my/mapService/config/SpringConfig.java
+++ src/main/java/com/my/mapService/config/SpringConfig.java
@@ -1,7 +1,10 @@
+
 package com.my.mapService.config;
 
+import com.my.mapService.repository.ListUserRepository;
 import com.my.mapService.repository.MapMemberRepository;
 import com.my.mapService.repository.MemberRepository;
+import com.my.mapService.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +13,10 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         return new MapMemberRepository();
+    }
+
+    @Bean
+    public UserRepository userRepository() {
+        return new ListUserRepository();
     }
 }
